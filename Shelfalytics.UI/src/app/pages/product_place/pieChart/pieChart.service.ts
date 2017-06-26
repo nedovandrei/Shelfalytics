ba470a2a6896a9422a2c28c43a8898d0ca58f124
komp <meduzameduzov1995@gmail.com>
2017-06-26 22:24:46 +0300

    add ng2-theme

---
+++ Shelfalytics.UI/src/app/pages/product_place/pieChart/pieChart.service.ts
@@ -0,0 +1,26 @@
+import {Injectable} from '@angular/core';
+import {BaThemeConfigProvider, colorHelper} from '../../../theme';
+
+@Injectable()
+export class PieChartService {
+
+  constructor(private _baConfig:BaThemeConfigProvider) {
+  }
+
+  getData() {
+    let pieColor = this._baConfig.get().colors.custom.dashboardPieChart;
+    return [
+      {
+        color: pieColor,
+        description: 'dashboard.new_visits',
+        stats: '57,820',
+        icon: 'person',
+      }, {
+        color: pieColor,
+        description: 'dashboard.purchases',
+        stats: '$ 89,745',
+        icon: 'money',
+      }
+    ];
+  }
+}
