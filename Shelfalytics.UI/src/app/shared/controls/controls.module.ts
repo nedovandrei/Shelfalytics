82b96083ec1f7c6daeec1167099cdf767bd67486
komp <andrey1nedov@gmail.com>
2017-07-16 21:48:54 +0300

    add component modal for shared & change text for page point of sales

--- Shelfalytics.UI/src/app/shared/controls/controls.module.ts
+++ Shelfalytics.UI/src/app/shared/controls/controls.module.ts
@@ -3,18 +3,22 @@ import { CommonModule } from '@angular/common';
 import { Ng2SmartTableModule } from 'ng2-smart-table';
 import { NgaModule } from '../../theme/nga.module';
 import {NgGridModule, NgGridItem, NgGridConfig, NgGridItemConfig, NgGridItemEvent} from 'angular2-grid';
+import { NgbModalModule } from '@ng-bootstrap/ng-bootstrap';
+
 import { GridstackComponent } from './gridstack/gridstack.component';
 import { SmartTableComponent } from './smart-table/smart-table.component';
 import { AngularGridComponent } from './angular2-grid/angular2-grid.component';
+import { DefaultModalComponent } from './default-modal/default-modal.component';
 
 @NgModule({
   imports: [
     CommonModule,
     Ng2SmartTableModule,
     NgaModule,
-    NgGridModule
+    NgGridModule,
+    NgbModalModule
   ],
-  declarations: [GridstackComponent, SmartTableComponent, AngularGridComponent],
-  exports: [GridstackComponent, SmartTableComponent, AngularGridComponent],
+  declarations: [GridstackComponent, SmartTableComponent, AngularGridComponent, DefaultModalComponent],
+  exports: [GridstackComponent, SmartTableComponent, AngularGridComponent, DefaultModalComponent],
 })
 export class ControlsModule { }
