818e154a43e4ee69dbdad43b7d2e3cf0cfc4f40d
komp <andrey1nedov@gmail.com>
2017-07-23 23:57:52 +0300

    add component datapicker

--- Shelfalytics.UI/src/app/shared/controls/controls.module.ts
+++ Shelfalytics.UI/src/app/shared/controls/controls.module.ts
@@ -1,17 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { FormsModule, ReactiveFormsModule, NG_VALUE_ACCESSOR, ControlValueAccessor} from "@angular/forms";
 import { Ng2SmartTableModule } from 'ng2-smart-table';
 import { NgaModule } from '../../theme/nga.module';
 import { NgGridModule, NgGridItem, NgGridConfig, NgGridItemConfig, NgGridItemEvent} from 'angular2-grid';
 import { NgbModalModule } from '@ng-bootstrap/ng-bootstrap';
 import { RouterModule } from "@angular/router";
+import { SlimScrollModule } from 'ng2-slimscroll';
+import { SlimScrollOptions } from 'ng2-slimscroll';
 
 import { GridstackComponent } from './gridstack/gridstack.component';
 import { SmartTableComponent } from './smart-table/smart-table.component';
 import { AngularGridComponent } from './angular2-grid/angular2-grid.component';
 import { ADTableComponent } from "./adtable/adtable.component";
 import { DefaultModalComponent } from './default-modal/default-modal.component';
+import { DatePickerComponent } from './ng2-datepicker/ng2-datepicker.component';
 
 @NgModule({
   imports: [
@@ -22,15 +25,17 @@ import { DefaultModalComponent } from './default-modal/default-modal.component';
     NgbModalModule,
     FormsModule,
     ReactiveFormsModule,
-    RouterModule
+    RouterModule,
+    SlimScrollModule
   ],
   declarations: [
-    GridstackComponent, 
-    SmartTableComponent, 
-    AngularGridComponent, 
-    DefaultModalComponent, 
-    ADTableComponent
+    GridstackComponent,
+    SmartTableComponent,
+    AngularGridComponent,
+    DefaultModalComponent,
+    ADTableComponent,
+    DatePickerComponent
   ],
-  exports: [GridstackComponent, SmartTableComponent, AngularGridComponent, DefaultModalComponent, ADTableComponent],
+  exports: [GridstackComponent, SmartTableComponent, AngularGridComponent, DefaultModalComponent, ADTableComponent, DatePickerComponent, SlimScrollModule, FormsModule],
 })
 export class ControlsModule { }
