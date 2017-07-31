a2073060c2025f0c289a789b4951d586b8202793
komp <andrey1nedov@gmail.com>
2017-08-01 00:04:51 +0300

    add icon for post-info

--- Shelfalytics.UI/src/app/pages/main/main.component.ts
+++ Shelfalytics.UI/src/app/pages/main/main.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { IGoogleMapsData, ICoordinates, IMarker } from "../../shared/maps/google-maps/google-maps.model";
 
 @Component({
   selector: 'app-main',
@@ -6,7 +7,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./main.component.scss']
 })
 export class MainComponent implements OnInit {
-
+  private mapSettings: IGoogleMapsData = {
+    center: {
+      lat: 0,
+      lng: 0
+    },
+    // zoom: 16,
+    markers: [],
+    zoomControl: true
+  };
   constructor() { }
 
   ngOnInit() {
