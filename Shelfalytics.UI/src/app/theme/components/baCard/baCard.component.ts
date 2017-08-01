79d15cd4613330aa17e84e5bb68ef962809bf9ce
komp <andrey1nedov@gmail.com>
2017-08-02 02:06:29 +0300

    add scroll & change color scheme

--- Shelfalytics.UI/src/app/theme/components/baCard/baCard.component.ts
+++ Shelfalytics.UI/src/app/theme/components/baCard/baCard.component.ts
@@ -3,9 +3,11 @@ import {Component, Input} from '@angular/core';
 @Component({
   selector: 'ba-card',
   templateUrl: './baCard.html',
+  styleUrls: ["./baCard.scss"]
 })
 export class BaCard {
   @Input() title:String;
+  @Input() navTabs:String;
   @Input() baCardClass:String;
   @Input() cardType:String;
 }
