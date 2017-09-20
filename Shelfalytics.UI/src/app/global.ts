263c9daedfef7c235a54083d01662631cdbeb015
Hatebreeder <dee.snake@gmail.com>
2017-09-20 16:27:24 +0300

    fixed UI issue with equipment Fill chart

--- Shelfalytics.UI/src/app/global.ts
+++ Shelfalytics.UI/src/app/global.ts
@@ -3,10 +3,10 @@ import { Injectable } from "@angular/core";
 import { GlobalFilter } from "./shared/services/global-filter.service";
 
 export const global = {
-    apiPath: "http://localhost:6070/api/",
+    // apiPath: "http://localhost:6070/api/",
     // apiPath: "http://ithell-001-site1.ctempurl.com/api/", // DEV SERVER
     // apiPath: "http://shelfalytics-001-site2.gtempurl.com/api/", // UAT SERVER
-    // apiPath: "http://api.shelf.work/api/", // UAT SERVER 2
+    apiPath: "http://api.shelf.work/api/", // UAT SERVER 2
     imagePath: "http://api.shelf.work/Content/photos/",
     googleMapsApiKey: "AIzaSyDAVKaScBsoz10FfT-hobo1IMHYp-zssvg"
 };
