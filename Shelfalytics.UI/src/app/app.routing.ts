5c44e5309ea54472134eca5c4be00c727eae4c34
Hatebreeder <dee.snake@gmail.com>
2017-09-23 11:34:43 +0300

    fixes

--- Shelfalytics.UI/src/app/app.routing.ts
+++ Shelfalytics.UI/src/app/app.routing.ts
@@ -6,4 +6,4 @@ export const routes: Routes = [
   { path: "**", redirectTo: "pages/main" }
 ];
 
-export const routing: ModuleWithProviders = RouterModule.forRoot(routes);
+export const routing: ModuleWithProviders = RouterModule.forRoot(routes, { useHash: true });
