dec1eda7e4dd4c581c669e234e64fe635150bbf2
Hatebreeder <dee.snake@gmail.com>
2017-09-29 18:52:08 +0300

    realtime data for pushers

--- Shelfalytics.UI/src/app/pages/points-of-sale/pos-info/equipment-info/equipment-info.component.ts
+++ Shelfalytics.UI/src/app/pages/points-of-sale/pos-info/equipment-info/equipment-info.component.ts
@@ -19,22 +19,38 @@ export class EquipmentInfoComponent implements OnInit, OnChanges {
   private initFlag: boolean = false;
   private equipmentData: any;
   private timeStamp: string;
+  private firstLoad: boolean = true;
 
   ngOnInit() {
 
   }
 
   ngOnChanges() {
-    this.initFlag = false;
-    this.init();
+    if(this.firstLoad){
+      this.init();
+    } else {
+      this.reload();
+    }
+    
   }
 
   private init() {
+    this.initFlag = false;
     this.posInfoService.getShelfData(this.equipmentId).subscribe((data: any) => {
       this.equipmentData = data[0];
       console.log("equipment info ", data);
       this.timeStamp = moment(data[0].TimeStamp).format("hh:mm:ss A, dddd, Do MMMM YYYY");
       this.initFlag = true;
+      this.firstLoad = false;
+    });
+
+  }
+
+  private reload(){
+    this.posInfoService.getShelfData(this.equipmentId).subscribe((data: any) => {
+      this.equipmentData = data[0];
+      console.log("equipment info ", data);
+      this.timeStamp = moment(data[0].TimeStamp).format("hh:mm:ss A, dddd, Do MMMM YYYY");
     });
   }
 
